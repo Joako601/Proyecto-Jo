0610fe03d9ab520ad7748dc5f72b61abf81fbc2b
Joako601 <joaquin.uriona@tecdesoftware.edu.mx>
2026-05-24 19:45:30 -0600

    feat: add Menu and Ubicacion controllers and views

---
+++ Projecto Jo'/Controllers/historia/HistoriaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Proyecto_Jo_.Controllers
+{
+	public class HistoriaController : Controller
+	{
+		public IActionResult Historia()
+		{
+			return View();
+		}
+	}
+}
---
+++ Projecto Jo'/Controllers/ubicacion/UbicacionControllercs.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Proyecto_Jo_.Controllers
+{
+	public class UbicacionController : Controller
+	{
+		public IActionResult Ubicacion()
+		{
+			return View(); 
+		}
+	}
+}

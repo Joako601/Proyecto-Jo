1591e01033881999bba050517e882b9071c27618
Joako601 <joaquin.uriona@tecdesoftware.edu.mx>
2026-06-24 08:54:01 -0600

    feat(api): crear endpoint para Proyecto Jo

--- Projecto Jo'/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Proyecto_Jo_.Models
-{
-	public class ErrorViewModel
-	{
-		public string? RequestId { get; set; }
-
-		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-	}
-}
